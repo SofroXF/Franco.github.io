8eb10fc0e4550da47250d00155142ba5cabc368e
SofroXF <157041275+SofroXF@users.noreply.github.com>
2025-02-16 19:17:20 -0500

    Update and rename ipget.js to script.js

--- ipget.js
+++
@@ -1,9 +0,0 @@
-var request = new XHMLHttpRequest();
-
-request.open('GET', 'https://www.extreme-ip-lookup.com/json', true);
-request.onload = function(){
-  var data = JSON.parse(this.response);
-  var test = document.getElementById("test");
-  test.innerHTML = data;
-}
-request.send();
